bd802156226c951b2768c0ba03ddc28ddc1f397a
Eugene Shwab <chillofoctober@gmail.com>
2012-06-08 15:17:17 +0400

    Translate to russian language

---
+++ lang/ru/qtype_ddimageortext.php
@@ -0,0 +1,66 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Local language pack from http://www.moodle.vsu.ru
+ *
+ * @package    qtype
+ * @subpackage ddimageortext
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['addmoredropzones'] = 'Заготовки для ещё {no} областей для перетаскивания';
+$string['addmoreimages'] = 'Заготовки для ещё {no} перетаскиваемых элементов';
+$string['answer'] = 'Ответ';
+$string['bgimage'] = 'Фоновое изображение';
+$string['correctansweris'] = 'Верный ответ: {$a}';
+$string['draggableimage'] = 'Перетаскиваемое изображение';
+$string['draggableitem'] = 'Перетаскиваемый элемент';
+$string['draggableitemheader'] = 'Перетаскиваемый элемент {$a}';
+$string['draggableitemtype'] = 'Тип';
+$string['draggableword'] = 'Перетаскиваемый текст';
+$string['dropbackground'] = 'Фоновое изображение для перетаскивания маркетов на него';
+$string['dropzone'] = 'Зона для перетаскивания {$a}';
+$string['dropzoneheader'] = 'Зоны для перетаскивания';
+$string['formerror_disallowedtags'] = 'Вы используете здесь теги html, не разрешенные в тексте внутри перетаскиваемых элементов';
+$string['formerror_multipledraginstance'] = 'Вы выбрали это изображение {$a} несколько раз как верный вариант ответа в качестве области для перетаскивания, но оно не отмечено как изображение с возможностью перетаскивать несколько раз';
+$string['formerror_multipledraginstance2'] = 'Вы выбрали это изображение несколько раз как верный ответ в качестве зоны для перемещения, но оно не отмечено как элемент с возможностью перетаскивать несколько раз';
+$string['formerror_noallowedtags'] = 'html теги не разрешены в этом тексте, т.к. это альтернативный тест для перетаскиваемого изображения';
+$string['formerror_nobgimage'] = 'Вам нужно выбрать изображение для использования как фон с областями для перетаскивания';
+$string['formerror_nofile'] = 'Вам нужно загрузить или выбрать файл для использования здесь';
+$string['formerror_nofile3'] = 'Вам нужно здесь выбрать файл изображения, или удалить ассоциированную метку и снять галочку "несколько"';
+$string['formerror_noimageselected'] = 'Вам нужно выбрать перетаскиваемый элемент, который будет верным вариантом для этой зоны размещения';
+$string['formerror_noxleft'] = 'Вам нужно ввести значение x координат для верхнего левого угла этой зоны для размещения. Вы можете перетаскивать эту зону, тем самым устанавливая координаты, или ввести их вручную';
+$string['formerror_noytop'] = 'Вам нужно ввести значение y координат для верхнего левого угла этой зоны для размещения. Вы можете перетаскивать эту зону, тем самым устанавливая координаты, или ввести их вручную';
+$string['infinite'] = 'Несколько';
+$string['label'] = 'Текст';
+$string['nolabel'] = 'Нет текстовой метки';
+$string['pleasedraganimagetoeachdropregion'] = 'Ваш ответ не заполнен, пожалуйста перетащите элемент в каждый регион для размещения';
+$string['pluginname'] = 'Перетаскивание на изображение';
+$string['pluginname_help'] = 'Выберите файл с фоновым изображением, выберите изображения для перетаскивания или просто введите текст и определите зоны размещения на фоновом изображении, у которого будет возможность перетаскиваться.';
+$string['pluginnameadding'] = 'Добавить перетаскивание на изображение';
+$string['pluginnameediting'] = 'Редактировать перетаскивание на изображение';
+$string['pluginnamesummary'] = 'Изображения или текстовые метки перетаскиваются в выделенные области на фоновом изображении';
+$string['previewarea'] = 'Область предпросмотра -';
+$string['previewareaheader'] = 'Предпросмотр';
+$string['previewareamessage'] = 'Выберите файл с фоновым изображением и выберите изображения для перетаскивания или просто введите текст, у которого будет возможность перетаскиваться. Затем выберите перемещаемый элемент для каждой \'зоны расположения\', и перетащите перемещаемый элемент туда, куда должны их перетаскивать студенты';
+$string['refresh'] = 'Обновить предварительный просмотр';
+$string['shuffleimages'] = 'Перемешивать перетаскиваемые элементы при каждой попытке';
+$string['summarisechoiceno'] = 'Элемент {$a}';
+$string['summariseplaceno'] = 'Выделенная область {$a}';
+$string['xleft'] = 'Слева';
+$string['ytop'] = 'Сверху';
